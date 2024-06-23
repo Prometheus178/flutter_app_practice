7e41d99ed6e31a19cb0283f903556d510954d798
Sergey <sergeipopof178@gmail.com>
2024-06-23 19:35:31 +0300

    removed

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_practice/src/page/setting_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
